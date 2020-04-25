d629410ae4cdf8252c1876fbd9f20937c442180f
Rosa Carranza <rcarranza1024@yahoo.com>
2020-04-25 21:07:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe 
+  attr_accessor :color :size :material
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def condition
+    puts "shoe has been repaired"
+  end
+end
